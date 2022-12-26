1368f363e7bae34e9f66def94e14ca5857c3676f
Moberd <ocheretov.ilya@yandex.ru>
2022-12-27 00:49:00 +0300

    making tests

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 def factorial(n):
     if n == 1:
         return 1
+    elif n == 0:
+        return 1
     else:
         return n * factorial(n-1)
 
 
 if __name__ == '__main__':
-    print(factorial())
+    print(factorial(10))
